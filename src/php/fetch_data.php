3f7b023d2fa904a27fcc52b260c46789cad0fd34
ukonpower <obshi0120@icloud.com>
2019-07-01 15:24:42 +0900

    Secret

--- src/php/fetch_data.php
+++ src/php/fetch_data.php
@@ -1,13 +1,14 @@
 <?php
 
     header("Content-Type: application/json; charset=UTF-8");
+    require('login.php');
 
     $data = [];
     $atrium = [];
     $lounge = [];
     $syokudo = [];
-
-    $conn = mysqli_connect("localhost","organic_select","organic_MySQL","organic");
+    
+    $conn = mysqli_connect( $db['host'],$db['user'],$db['pass'],$db['dbname'] );
     $res = mysqli_query($conn,"SELECT management_id as 管理ID, congestion_rate as 混雑割合, insertion_time as 挿入時刻 FROM atrium_count ORDER BY 挿入時刻 DESC;");
 
     while($row = mysqli_fetch_array($res)){
---
+++ src/php/login.php
@@ -0,0 +1,7 @@
+<?php
+	$conn = mysqli_connect("localhost","organic_select","organic_MySQL","organic");
+	$db['host'] = "localhost"; 
+	$db['user'] = "organic_select"; 
+	$db['pass'] = "organic_MySQL"; 
+	$db['dbname'] = "organic"; 
+?>
